88ed3a7306b3fb6dd22d429804bf43fb71d8f9c5
andbilous-digis <andbilous@gmail.com>
2020-04-14 23:43:59 +0300

    Updates

---
+++ precache-manifest.a5959d24c1d5f49520e6165270733466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c9c9f9be39f6b39b1afa5b9f1503fe",
+    "url": "/cursor-router/index.html"
+  },
+  {
+    "revision": "69081736d0e8cdb7b4d8",
+    "url": "/cursor-router/static/css/main.43386950.chunk.css"
+  },
+  {
+    "revision": "d87a0ca5597348ee0210",
+    "url": "/cursor-router/static/js/2.649f9478.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cursor-router/static/js/2.649f9478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69081736d0e8cdb7b4d8",
+    "url": "/cursor-router/static/js/main.8783a319.chunk.js"
+  },
+  {
+    "revision": "df9cbe0adec444ee51b5",
+    "url": "/cursor-router/static/js/runtime-main.02bae649.js"
+  }
+]);
